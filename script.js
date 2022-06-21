822bc86ebf1a0ef5e9bdfe70eb3488213ed43c31
Adrian Dziergas <adriandziergas404@gmail.com>
2022-06-21 19:14:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const name = 'lisa';
+console.log('name');
+let age = 23
+let late = 20
+console.log(late);
+const life = 28
+
+const Myname= 'Adrian'
+const fovMeal = 'schabowy'
+console.log('Mam na imię ' + Myname + ' moim ulubionym daniem jest ' + fovMeal );
