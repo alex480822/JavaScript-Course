58e8af4661f8171e2b865fd07190404338ece09f
Adrian Dziergas <adriandziergas404@gmail.com>
2022-06-22 22:26:50 +0200

    second  commit

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ const life = 28
 const Myname= 'Adrian'
 const fovMeal = 'schabowy'
 console.log('Mam na imię ' + Myname + ' moim ulubionym daniem jest ' + fovMeal );
+
+const lastName = 'Moje Nazwisko';
+const letters = 10;
+console.log(lastName);
+console.log(lastName + 'zawiera ' + letters + ' liter')
