c089fb5285fbb66e960637b7f0248d4953dfc4ce
Adrian Dziergas <adriandziergas404@gmail.com>
2022-06-23 22:12:36 +0200

    third  commit

--- script.js
+++ script.js
@@ -13,3 +13,33 @@ const lastName = 'Moje Nazwisko';
 const letters = 10;
 console.log(lastName);
 console.log(lastName + 'zawiera ' + letters + ' liter')
+// MUSI BYĆ W GRAWISACH
+console.log(`Cześć, jestem   ${Myname} `)
+
+
+// Zadanie
+// // const firstName = 'Tomek'
+// age = 12
+// age = 18
+ 
+// const favColor = 'niebieski'
+// const favMeal = 'schabowy'
+ 
+// let currentCar
+// currentCar = 'Audi'
+ 
+// let firstNameSecond = 'Ania'
+// let ageSecond = 24
+// let ulubionyKolor = 'czerwony'
+
+const msg ='to jest testowa wiadomość';
+
+const newMsg = msg.toUpperCase();
+console.log(newMsg.toLowerCase());
+
+const date = new Date()
+console.log(date.toLocaleDateString('en'))
+
+const userName = 'Maciej'
+const newUsername = userName.charAt(0).toUpperCase() + userName.slice(1);
+console.log(newUsername)
