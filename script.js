87becbb426e736b4b21669be5cb314f4c8abca64
Adrian Dziergas <adriandziergas404@gmail.com>
2022-06-26 21:34:01 +0200

    five  commit

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
-const name = 'lisa';
-console.log('name');
-let age = 23
-let late = 20
+const name = "lisa";
+console.log("name");
+let age = 23;
+let late = 20;
 console.log(late);
-const life = 28
+const life = 28;
 
-const Myname= 'Adrian'
-const fovMeal = 'schabowy'
-console.log('Mam na imię ' + Myname + ' moim ulubionym daniem jest ' + fovMeal );
+const Myname = "Adrian";
+const fovMeal = "schabowy";
+console.log("Mam na imię " + Myname + " moim ulubionym daniem jest " + fovMeal);
 
-const lastName = 'Moje Nazwisko';
+const lastName = "Moje Nazwisko";
 const letters = 10;
 console.log(lastName);
-console.log(lastName + 'zawiera ' + letters + ' liter')
+console.log(lastName + "zawiera " + letters + " liter");
 // MUSI BYĆ W GRAWISACH
-console.log(`Cześć, jestem   ${Myname} `)
-
+console.log(`Cześć, jestem   ${Myname} `);
 
 // Zadanie
 // const firstName = 'Tomek'
-age = 12
-age = 18
- 
-const favColor = 'niebieski'
-const favMeal = 'schabowy'
- 
-let currentCar
-currentCar = 'Audi'
- 
-let firstNameSecond = 'Ania'
-let ageSecond = 24
-let ulubionyKolor = 'czerwony'
+age = 12;
+age = 18;
 
+const favColor = "niebieski";
+const favMeal = "schabowy";
 
+let currentCar;
+currentCar = "Audi";
+
+let firstNameSecond = "Ania";
+let ageSecond = 24;
+let ulubionyKolor = "czerwony";
 
 //Zadanie
 
-const msg ='to jest testowa wiadomość';
+// const msg = "to jest testowa wiadomość";
 
-const newMsg = msg.toUpperCase();
-console.log(newMsg.toLowerCase());
+// const newMsg = msg.toUpperCase();
+// console.log(newMsg.toLowerCase());
 
-const date = new Date()
-console.log(date.toLocaleDateString('en'))
+// const date = new Date();
+// console.log(date.toLocaleDateString("en"));
 
-const userName = 'Maciej'
-const newUsername = userName.charAt(0).toUpperCase() + userName.slice(1);
-console.log(newUsername)
+// const userName = "Maciej";
+// const newUsername = userName.charAt(0).toUpperCase() + userName.slice(1);
+// console.log(newUsername);
