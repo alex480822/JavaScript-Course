463fede6d954d4a479e2cfa8e1a38076733357c9
Adrian Dziergas <adriandziergas404@gmail.com>
2022-07-04 19:30:12 +0200

    task completed

--- script.js
+++ script.js
@@ -41,6 +41,33 @@ console.log(newMsg.toLowerCase());
 const date = new Date();
 console.log(date.toLocaleDateString("en"));
 
-const userName = "Maciej";
+const userName = "Adrian";
 const newUsername = userName.charAt(0).toUpperCase() + userName.slice(1);
 console.log(newUsername);
+
+//Zadanie
+/*
+    Metody do wykorzystania:
+    charAt()
+    includes()
+    replace()* podchwytliwe 🙂 
+    slice()
+    split()
+    toLowerCase()
+    toUpperCase()
+*/
+ 
+const text1 = 'powiększ mnie!'
+console.log(text1.toUpperCase());
+const text2 = 'ZAPISZ MNIE MAŁYMI LITERAMI'
+console.log(text2.toLowerCase());
+const text3 = '$#%#^ wytnij te dziwne znaki na początku!'
+console.log(text3.slice(5));
+const text4 = 'sprawdź, czy zawieram słowo "czy"'
+console.log(text4.includes('czy'));
+const text5 = 'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"'
+console.log(text5.charAt(2));
+const text6 = 'pies zamień każde słowo pies, na słowo kot pies'
+console.log(text6.replaceAll('pies', 'kot'))
+const text7 = 'podziel, ten, string, od, przecinków'
+console.log(text7.split(' '))
